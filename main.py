bc2e2e9c3f2507a07f3d941da138816e70945393
alessandro-maccario <alessandro.maccario@gmail.com>
2021-10-12 15:38:07 +0200

    First Exercise: DONE!

--- main.py
+++ main.py
@@ -1,27 +1,19 @@
 """
+    EXERCISE 1 - Information Search and Recommendation System Course
+
     Write a program that determines the mean rating in the dataset in the following way:
         - Create a list of data type “float” to store all ratings in memory.
-        - Open the file “ratings.csv” and read the contents line by line. --> DONE
+        - Open the file “ratings.csv” and read the contents line by line.
         - Store each rating in the list.
         - Close the file.
         - Iterate through the resulting list, sum up the values and calculate the average at the end.
         - Print the result.
 """
 
-
-import numpy as np
-import pandas as pd
-
 # OPEN THE FILE “RATINGS.CSV” AND READ THE CONTENTS LINE BY LINE.
 f = open('ratings.csv', 'r')
 
 lines = f.readlines()
-
-# result = list()
-# For loop
-# for x in lines:
-#     result.append(x.split(',')[2])
-# f.close()
 
 # Store each rating in the list. (List Comprehension)
 result = [x.split(',')[2] for x in lines]
@@ -31,14 +23,21 @@ f.close()
 
 # Iterate through the resulting list, sum up the values and calculate the average at the end.
 
-def summa_list(l):
+
+def summa_list(list_of_rating):
     summa = 0
-    new_list = [result[0]] + [float(i) for i in l[1:]]
+    new_list = [list_of_rating[0]] + [float(i) for i in list_of_rating[1:]]
+
+    count = 0
     for x in new_list[1:]:
         summa += x
-    average_rating = summa/len(l)
+        count += 1
+
+    average_rating = float(round(summa/count))
     return average_rating
+
 
 # Print the result.
 print(summa_list(result))
 
+# END
