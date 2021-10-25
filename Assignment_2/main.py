06e4304b081d34e500b35469dd212c99ad73e683
alessandro-maccario <alessandro.maccario@gmail.com>
2021-10-25 23:48:08 +0200

    Task 3: DONE!

--- Assignment_2/main.py
+++ Assignment_2/main.py
@@ -1,3 +1,4 @@
+import numpy
 import numpy as np
 import pandas as pd
 
@@ -81,7 +82,7 @@ df_data_sorted = df_data.sort_values(by=['popularity'])
 # print(df_data_sorted)
 
 # Print the popularity values.
-print(df_data_sorted['popularity'])
+# print(df_data_sorted['popularity'])
 
 
 """
@@ -98,6 +99,7 @@ print(df_data_sorted['popularity'])
                         print(type(df))
                 Print the information about the first and the last movie in the dataset.
                 Show the information about the movie “Jumanji”.
+        
         Create a smaller DataFrame called small_df from the given one by considering only the 
         following columns: 'title', 'release_date', 'popularity', 'revenue', 
         'runtime' and 'genres',
@@ -125,3 +127,80 @@ print(df_data_sorted['popularity'])
         Now, print the titles of all movies that were released after the year 2010. 
         
 """
+
+# Write a program that does the following:
+#       Read the CSV file “movies_metadata” into a Pandas DataFrame.
+#       Use the type function to inspect the DataFrame, i.e, inspect the output of the command:
+#               print(type(df))
+#               Print the information about the first and the last movie in the dataset.
+#               Show the information about the movie “Jumanji”.
+
+movie_df = pd.read_csv('archive/movies_metadata.csv', low_memory=False)
+# print(type(df))
+# print(type(movie_df))
+
+# Print the information about the first and the last movie in the dataset.
+# print(movie_df.iloc[0])
+# print(movie_df.iloc[-1])
+
+# Check if the last element is the one gave from the last line of code
+# print(movie_df.tail()['title'])
+
+# Show the information about the movie “Jumanji”.
+# Do not trunc the row by setting 'display.width'
+pd.set_option('display.width', None)
+jumanji = movie_df[movie_df['original_title'] == 'Jumanji']
+# print(jumanji)
+
+"""
+        Create a smaller DataFrame called small_df from the given one by considering only the 
+        following columns: 'title', 'release_date', 'popularity', 'revenue', 
+        'runtime' and 'genres',
+"""
+
+small_df = movie_df.copy()[['title', 'release_date', 'popularity', 'revenue', 'runtime', 'genres']]
+# print(small_df)
+
+"""
+        Create a function “to_float” to convert the type of its input to float with following code: 
+        
+        def to_float(x): 
+                try: 
+                        x = float(x) 
+                except: 
+                        x = numpy.nan 
+                return x   
+"""
+
+def to_float(x):
+        try:
+                x = float(x)
+        except:
+                x = np.nan
+
+        return x
+
+"""
+        Next, add the following code to your program that adds a column names ‘release_year’ 
+        to the DataFrame. Inspect how the lambda function is working.
+        
+        small_df = df[['title', 'release_date', 'popularity', 'revenue', 'runtime', 'genres']].copy() 
+        small_df.loc['release_date'] = pd.to_datetime(small_df['release_date'], errors='coerce') 
+        small_df['release_year'] = small_df['release_date'].apply
+                (lambda x: str(x).split('-')[0] if x != numpy.nan else numpy.nan) 
+        small_df['release_year'] = small_df['release_year'].apply(to_float) 
+        small_df['release_year'] = small_df['release_year'].astype('float') 
+        small_df = small_df.drop(columns="release_date")
+        
+        Now, print the titles of all movies that were released after the year 2010. 
+"""
+
+small_df = movie_df[['title', 'release_date', 'popularity', 'revenue', 'runtime', 'genres']].copy()
+small_df.loc['release_date'] = pd.to_datetime(small_df['release_date'], errors='coerce')
+small_df['release_year'] = small_df['release_date'].apply(lambda x: str(x).split('-')[0] if x != np.nan else np.nan)
+small_df['release_year'] = small_df['release_year'].apply(to_float)
+small_df['release_year'] = small_df['release_year'].astype('float')
+small_df = small_df.drop(columns='release_date')
+
+# Now, print the titles of all movies that were released after the year 2010.
+print(small_df[small_df['release_year'] > 2010.0])
